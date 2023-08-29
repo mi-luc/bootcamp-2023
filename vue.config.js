7886b78d7a4933fc16614b13f5f9d790565e48dd
unknown <luca.mitran@gmail.com>
2023-08-29 10:06:48 +0100

    Aproape gata :)

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/bootcamp-sts-2023/' // Numele repository-ului creat.
+    ? '/bootcamp-2023/' // Numele repository-ului creat.
     : '/'
 })
